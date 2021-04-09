6f4fe7ab13c4835b247f98a519da29e2cfd27161
Ali Agboola <aliagboola1@gmail.com>
2021-04-09 18:32:08 +0100

    add category keys

---
+++ python_task4/task4.py
@@ -0,0 +1,24 @@
+class Budget:
+    
+    def __init__(self, food, clothing, entertainment, savings, miscellaneous, rent, utilities):
+        self.food = food
+        self.clothing = clothing
+        self.entertainment = entertainment
+        self.savings = savings
+        self.miscellaneous = miscellaneous
+        self.rent = rent
+        self.utilities = utilities
+    
+    def withdraw(self, amount):
+        pass
+    
+    def deposit(self, amount):
+        pass
+    
+    def transfer(self, source, destination, amount):
+        pass
+    
+    def check_balance(self, amount):
+        pass
+    
+    
