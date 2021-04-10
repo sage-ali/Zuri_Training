055fe8c82f48e268f6ffc7fac562a97d5f9a8038
Ali Agboola <aliagboola1@gmail.com>
2021-04-10 06:11:15 +0100

    add the attributes and methods

--- python_task4/task4.py
+++ python_task4/task4.py
@@ -1,24 +1,32 @@
 class Budget:
     
-    def __init__(self, food, clothing, entertainment, savings, miscellaneous, rent, utilities):
-        self.food = food
-        self.clothing = clothing
-        self.entertainment = entertainment
-        self.savings = savings
-        self.miscellaneous = miscellaneous
-        self.rent = rent
-        self.utilities = utilities
+    def __init__(self, category):
+        self.category = category
+        self.amount = 0
     
     def withdraw(self, amount):
-        pass
-    
+        if amount <= self.amount:
+            self.amount -= amount
+            print(f"You've withdrawn {amount} from the {self.category} category")
+            print(self.check_balance())
+        else:
+            print(f"Insufficient balance in {self.category} category")    
+            
     def deposit(self, amount):
-        pass
+        self.amount += amount
+        print(f"You've successfully deposited {amount} to the {self.category} category")
+        print(self.check_balance())
+        
+    def transfer(self, destination, amount):
+        if amount <= self.amount:
+            self.amount -= amount
+            destination.amount += amount
+            print(f"You've successfully transfered {amount} from the {self.category} category to {destination.category} category")
+            print(f"{self.check_balance()} while that of {destination.category} is {destination.amount}")
+        else:
+            print(f"Insufficient balance in {self.category} category, can not complete transfer")
     
-    def transfer(self, source, destination, amount):
-        pass
-    
-    def check_balance(self, amount):
-        pass
+    def check_balance(self):
+        return f"The current balance of the {self.category} category is {self.amount}"
     
     
